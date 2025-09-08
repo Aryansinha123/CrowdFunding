d0655ce7b2eaed7ac9469a5305c8156e739cde10
Aryan <sinhaaryan173@gmail.com>
2025-09-08 23:10:37 +0530

    1:10:00 Aryan

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,14 @@
 const hre = require("hardhat");
+//0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9
 async function main() {
   const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
-  const crowdFunding = (await CrowdFunding) - deploy();
-  await crowdFunding.deployed();
-  console.log("CrowdFunding deployed to ${crowdFunding-address}");
+  const crowdFunding = await CrowdFunding.deploy();
+  // await crowdFunding.deployed();
+  console.log(`CrowdFunding deployed to ${crowdFunding.target}`);
 }
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+//not according to yt video (changed because of new version)
